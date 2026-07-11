37cbc80c46a178670942127ec22aa21daa2e1b20
aryapandey1508 <aryapandey0555@gmail.com>
2026-07-11 16:27:17 +0530

    new commit added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let cors = require('cors');
 app.use(cors());
 app.get('/ping',(req,res)=> {
     res.send({
-        message :'hello, world!'
+        message :'hello, world!, welcome to gym'
     });
 });
 app.listen(port,()=>{
